f071210af924dd7dd49446430c8f70bf053c83d8
Adarsh <adarshv183@gmail.com>
2024-01-29 22:46:28 +0530

    peace:)

--- src/ExtendedGCD.java
+++ src/ExtendedGCD.java
@@ -1,38 +1,26 @@
 public class ExtendedGCD {
 
     // Helper class to store the result of the extended gcd
-    static class ExtendedGCDResult {
-        int gcd;
-        int x;
-        int y;
-
-        ExtendedGCDResult(int gcd, int x, int y) {
+    public static class GcdObject{
+        int gcd,x,y;
+        public GcdObject(int gcd, int x, int y){
             this.gcd = gcd;
             this.x = x;
             this.y = y;
         }
     }
-
-    // Function to perform extended gcd
-    static ExtendedGCDResult extendedGCD(int a, int b) {
-        if (b == 0) {
-            return new ExtendedGCDResult(a, 1, 0);
+    public static GcdObject computeGcd(int a, int b){
+        if(b==0){
+            return new GcdObject(a,1,0);
         }
-
-        ExtendedGCDResult previousResult = extendedGCD(b, a % b);
-        int x = previousResult.y;
-        int y = previousResult.x - (a / b) * previousResult.y;
-
-        return new ExtendedGCDResult(previousResult.gcd, x, y);
+        GcdObject prevObject = computeGcd(b, a%b);
+        int x = prevObject.y;
+        int y = prevObject.x - (a/b) * prevObject.y;
+        return new GcdObject(prevObject.gcd, x, y);
     }
-
     public static void main(String[] args) {
-        // Example usage
-        int a = 55;
-        int b = 80;
-
-        ExtendedGCDResult result = extendedGCD(a, b);
-        System.out.println("GCD(" + a + ", " + b + ") = " + result.gcd);
-        System.out.println("Coefficients (x, y) : (" + result.x + ", " + result.y + ")");
+        int a = 10, b = 25;
+        GcdObject res = computeGcd(a,b);
+        System.out.println("gcd:" + res.gcd + "\n\nx:" + res.x + "\n\ny:" + res.y);
     }
 }
