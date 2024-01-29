de1770244c0bd98bd05637a086b1a33720126a7c
Adarsh <adarshv183@gmail.com>
2024-01-29 23:48:12 +0530

    important changes were made!!

--- src/LCS.java
+++ src/LCS.java
@@ -4,28 +4,20 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class LCS {
-//    values for direction
-
-    private static int diagonal = 1;
-    private static int maxLength = 0;
-
-    private static Set<String> results = new HashSet<>();
-
+    private static final int diagonal = 1;
+    private static final Set<String> results = new HashSet<>();
     public static void main(String[] args) {
-        String s1 = "abaaba";
-        String s2 = "babbab";
+        String s1 = "abcd";
+        String s2 = "acbad";
         computeLcs(s1,s2);
     }
-
     private static void computeLcs(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
 //        cost matrix
-        int[][] cost = new int[n+1][m+1];
-        int[][] direction = new int[n+1][m+1];
+        int[][] cost = new int[s1.length()+1][s2.length()+1];
+        int[][] direction = new int[s1.length()+1][s2.length()+1];
 //        compute cost matrix
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++)  {
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++)  {
                 if(s1.charAt(i-1) == s2.charAt(j-1)){
                     cost[i][j] = cost[i-1][j-1] + 1;
                     direction[i][j] = diagonal;
@@ -34,7 +26,7 @@ public class LCS {
                 }
             }
         }
-        maxLength = cost[n][m];
+        int maxLength = cost[s1.length()][s2.length()];
 //        print cost matrix
         System.out.println("Cost Matrix");
         printMatrix(cost);
@@ -43,7 +35,7 @@ public class LCS {
         printMatrix(direction);
 
         System.out.println("\nMaximum length of LCS : " + maxLength);
-        printSequences(cost,direction,s1,s2,n,m,"");
+        printSequences(cost,direction,s1,s2,s1.length(),s2.length(),"");
 //        printing sequences
         for(String s : results){
             System.out.println(s);
@@ -54,25 +46,26 @@ public class LCS {
             System.out.println(Arrays.toString(a));
         }
     }
-    private static void printSequences(int[][] cost, int[][] direction, String s1, String s2, int n, int m,String seq) {
+    private static void printSequences(int[][] cost, int[][] direction, String s1, String s2, int m, int n,String seq) {
         if(n==0 || m ==0){
-            results.add(seq);
+            StringBuilder sb = new StringBuilder(seq);
+            sb.reverse();
+            results.add(sb.toString());
             return;
         }
         String helper = seq;
-        if(direction[n][m] == diagonal){
+        if(direction[m][n] == diagonal){
             helper += s1.charAt(m-1);
-            printSequences(cost,direction,s1,s2,n-1,m-1,helper);
+            printSequences(cost,direction,s1,s2,m-1,n-1,helper);
         }else {
-            if(cost[n-1][m] == cost[n][m-1]){
-                printSequences(cost,direction,s1,s2,n-1,m,helper);
-                printSequences(cost,direction,s1,s2,n,m-1,helper);
-            }else if(cost[n-1][m] == cost[n][m]){
-                printSequences(cost,direction,s1,s2,n-1,m,helper);
+            if(cost[m-1][n] == cost[m][n-1]){
+                printSequences(cost,direction,s1,s2,m-1,n,helper);
+                printSequences(cost,direction,s1,s2,m,n-1,helper);
+            }else if(cost[m-1][n] == cost[m][n]){
+                printSequences(cost,direction,s1,s2,m-1,n,helper);
             }else{
-                printSequences(cost,direction,s1,s2,n,m-1,helper);
+                printSequences(cost,direction,s1,s2,m,n-1,helper);
             }
-
         }
 
     }
